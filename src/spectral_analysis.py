85135b59eed96bb467a366b33efb862b663e3430
ozvar <OVardal@proton.me>
2024-05-31 15:51:17 +0800

    Additions to compute Fourier spectrum
    Changed SNR function to rely on FFT (not PSD) and added function to
    compute magnitudes of FFT.

--- src/spectral_analysis.py
+++ src/spectral_analysis.py
@@ -24,23 +24,44 @@ def compute_psd(
             verbose = verbose
     )
     if vROI:
-        spectrum.pick_channels(vROI)
+        spectrum.pick(vROI)
     psds, freqs = spectrum.get_data(return_freqs=True)
     return psds, freqs
 
 
-def snr_spectrum(
-        psd,
-        noise_n_neighbor_freqs: int,
-        noise_skip_neighbor_freqs: int
+def compute_fft_mag(
+    epochs,
+    analysis_params: dict
 ):
-    """Compute SNR spectrum from PSD spectrum using convolution.
+    """Compute response magnitude of fourier transformed epochs"""
+    data = epochs.get_data()
+    # Compute FFT for each epoch and channel
+    fft_output = np.fft.fft(data, axis=-1)
+    fft_mag = np.abs(fft_output)
+    # Slice the frequencies within our desired range
+    freqs = np.fft.fftfreq(data.shape[-1], 1/epochs.info['sfreq'])
+    freqs = freqs[
+            (freqs >= analysis_params['fmin']) &
+            (freqs <= analysis_params['fmax'])
+            ]
+    # Slice magnitudes up to the desired frequency
+    fft_mag = fft_mag[:, :, :len(freqs)]
+
+    return fft_mag, freqs
+
+
+def snr_spectrum(
+    fft,
+    noise_n_neighbor_freqs: int,
+    noise_skip_neighbor_freqs: int
+):
+    """Compute SNR spectrum from frequency spectrum using convolution.
 
     Parameters
     ----------
-    psd : ndarray, shape ([n_trials, n_channels,] n_frequency_bins)
-        Data object containing PSD values. Works with arrays as produced by
-        MNE's PSD functions or channel/trial subsets.
+    fft : ndarray, shape ([n_trials, n_channels,] n_frequency_bins)
+        Data object containing magnitude values obtained after FFT. Also works
+        with arrays as produced by MNE's PSD functions or channel/trial subsets.
     noise_n_neighbor_freqs : int
         Number of neighboring frequencies used to compute noise level.
         increment by one to add one frequency bin ON BOTH SIDES
@@ -68,16 +89,39 @@ def snr_spectrum(
     # Calculate the mean of the neighboring frequencies by convolving with the
     # averaging kernel.
     mean_noise = np.apply_along_axis(
-        lambda psd_: np.convolve(psd_, averaging_kernel, mode="valid"), axis=-1, arr=psd
+        lambda fft_: np.convolve(fft_, averaging_kernel, mode="valid"), axis=-1, arr=fft
     )
-    # The mean is not defined on the edges so we will pad it with nas. The
+    # The mean is not defined on the edges so we will pad it with nans. The
     # padding needs to be done for the last dimension only so we set it to
     # (0, 0) for the other ones.
     edge_width = noise_n_neighbor_freqs + noise_skip_neighbor_freqs
     pad_width = [(0, 0)] * (mean_noise.ndim - 1) + [(edge_width, edge_width)]
     mean_noise = np.pad(mean_noise, pad_width=pad_width, constant_values=np.nan)
+    snr = fft / mean_noise
+    snr = np.nan_to_num(snr, nan=0, posinf=0, neginf=0)
+    
+    return snr
 
-    return psd / mean_noise
+
+def compute_snr_for_epochs(
+    epochs,
+    analysis_params: dict,
+):
+    # Compute magnitudes of the FFT across epochs 
+    fft_mag, freqs = compute_fft_mag(
+            epochs=epochs,
+            analysis_params=analysis_params
+            )
+    # Compute the snr over all fft magnitudes
+    snr = snr_spectrum(
+            fft_mag,
+            analysis_params['noise_n_neighbor_bins'],
+            analysis_params['noise_skip_neighbor_bins']
+            )
+    # Now slice SNR up to the desired frequency
+    snr = snr[:, :, :len(freqs)]
+
+    return snr, freqs
 
 
 def compute_condition_psds_and_snrs(
