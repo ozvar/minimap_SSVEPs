451efc5d407d32072bb5bc4698a2a53e89ef61c5
ozvar <OVardal@proton.me>
2024-06-23 09:55:26 +0800

    Changed path specification
    Now pass epochs path directly rather than just the directory

--- code/src/utils.py
+++ code/src/utils.py
@@ -4,15 +4,19 @@ import os
 from pathlib import Path
 
 
-def load_epochs(epochs_dir: Path):
-    epochs_path = epochs_dir / "epochs.pickle"
+def load_epochs(epochs_path: Path):
     try:
-        with open(epochs_dir / "epochs.pickle", 'rb') as f:
+        with open(epochs_path, 'rb') as f:
             [left_minimap_epochs, right_minimap_epochs] = pickle.load(f)
         return left_minimap_epochs, right_minimap_epochs 
     except FileNotFoundError:
         print(f"Epochs file not found at {epochs_dir}")
         return None
+
+
+def load_data(data_dir:Path):
+    pass
+
 
 
 def setup_logger(
